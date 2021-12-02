04caa5d956666520f781840957bf053f81a21851
Nikshep <nikshep.nik92@gmail.com>
2021-12-02 20:47:58 +0530

    simulating the reset command

--- a.py
+++ a.py
@@ -1,6 +1,8 @@
 import os
 import csv
 import pondas
-print(os.getcwd())
-print("simulating the user code 2")
 
+print(os.getcwd())
+
+print("simulating the user code 2")
+print("simulating the user code 3 and trying the rest command")
